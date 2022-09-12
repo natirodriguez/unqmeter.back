07d0531a720d9cc8d3bddd71ddff8cd1f87b10ba
Matias Lugo <mlugo@algeiba.com>
2022-09-12 00:05:15 -0300

    Add try-catch in externalLogin method

--- UnqMeterAPI/Controllers/LoginController.cs
+++ UnqMeterAPI/Controllers/LoginController.cs
@@ -24,40 +24,44 @@ namespace UnqMeterAPI.Controllers
         [HttpPost("ExternalLogin")]
         public async Task<IActionResult> ExternalLogin([FromBody] ExternalAuth externalAuth)
         {
-            var payload = await _jwtHandler.VerifyGoogleToken(externalAuth);
-            if (payload == null)
-                return BadRequest("Invalid External Authentication.");
-
-            var info = new UserLoginInfo(externalAuth.Provider, payload.Subject, externalAuth.Provider);
-
-            var user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
-            if (user == null)
+            try
             {
-                user = await _userManager.FindByEmailAsync(payload.Email);
+                var payload = await _jwtHandler.VerifyGoogleToken(externalAuth);
+                if (payload == null)
+                    return BadRequest("Invalid External Authentication.");
+
+                var info = new UserLoginInfo(externalAuth.Provider, payload.Subject, externalAuth.Provider);
+
+                var user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
+                if (user == null)
+                {
+                    user = await _userManager.FindByEmailAsync(payload.Email);
+
+                    if (user == null)
+                    {
+                        user = new User { Email = payload.Email, UserName = payload.Email };
+                        await _userManager.CreateAsync(user);
+                        await _userManager.AddLoginAsync(user, info);
+                    }
+                    else
+                    {
+                        await _userManager.AddLoginAsync(user, info);
+                    }
+                }
 
                 if (user == null)
                 {
-                    user = new User { Email = payload.Email, UserName = payload.Email };
-                    await _userManager.CreateAsync(user);
-
-                    //prepare and send an email for the email confirmation
-
-                    await _userManager.AddToRoleAsync(user, "Viewer");
-                    await _userManager.AddLoginAsync(user, info);
+                    return BadRequest("Invalid External Authentication.");
                 }
-                else
-                {
-                    await _userManager.AddLoginAsync(user, info);
-                }
+
+                var token = await _jwtHandler.GenerateToken(user);
+                return Ok(new AuthResponse { Token = token, IsAuthSuccessful = true });
             }
-
-            if (user == null)
-                return BadRequest("Invalid External Authentication.");
-
-            //check for the Locked out account
-
-            var token = await _jwtHandler.GenerateToken(user);
-            return Ok(new AuthResponse { Token = token, IsAuthSuccessful = true });
+            catch (Exception e)
+            {
+                throw new Exception(e.StackTrace);
+            }
+            
         }
     }
 }
