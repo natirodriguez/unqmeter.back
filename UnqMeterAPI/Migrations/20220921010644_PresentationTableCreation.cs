f4d3b4cf39d8e8168e2d24a4e54f42445450f7b4
Matias Lugo <mlugo@algeiba.com>
2022-09-21 20:38:53 -0300

    Add Migration Presentations Table

---
+++ UnqMeterAPI/Migrations/20220921010644_PresentationTableCreation.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UnqMeterAPI.Migrations
+{
+    public partial class PresentationTableCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Presentaciones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UsuarioCreador = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Presentaciones", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Presentaciones");
+        }
+    }
+}
