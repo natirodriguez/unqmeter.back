8d8d6f2a151f338e35f1bc1b3ff35c30d44fb15b
Natalia Rodriguez <natalia.rodriguez@fluor.com>
2022-10-06 20:35:18 -0300

    Agrego servicio/test/controller para el usuario participante

---
+++ UnqMeterAPI/Services/RespuestaParticipanteService.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using UnqMeterAPI.Interfaces;
+using UnqMeterAPI.Models;
+
+namespace UnqMeterAPI.Services
+{
+    public class RespuestaParticipanteService : IRespuestaParticipanteService
+    {
+        private readonly IMapper _mapper;
+        private IRepositoryManager<Respuesta> _respuestaRepository;
+
+        public RespuestaParticipanteService(IMapper mapper, IRepositoryManager<Respuesta> respuestaRepository)
+        {
+            _mapper = mapper;
+            _respuestaRepository = respuestaRepository;
+        }
+
+    }
+}
---
+++ UnqMeterAPI/Test/RespuestaParticipanteServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Moq;
+using NUnit.Framework;
+using UnqMeterAPI.Interfaces;
+using UnqMeterAPI.Models;
+using UnqMeterAPI.Services;
+namespace UnqMeterAPI.Test
+{
+    [TestFixture]
+    public class RespuestaParticipanteServiceTest
+    {
+        private Mock<IMapper> _mockMapper;
+        private IRespuestaParticipanteService _respuestaParticipanteService;
+        private Mock<IRepositoryManager<Respuesta>> _repositoryRespuestaMocker;
+        IList<Respuesta> respuestas;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockMapper = new Mock<IMapper>();
+            _repositoryRespuestaMocker = new Mock<IRepositoryManager<Respuesta>>();
+
+            _respuestaParticipanteService = new RespuestaParticipanteService(_mockMapper.Object, _repositoryRespuestaMocker.Object);
+
+            respuestas = new List<Respuesta>();
+        }
+  
+    }
+}
