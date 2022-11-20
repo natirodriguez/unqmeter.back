8b1b5c4e8dfb8be10d3ecc08043f84b2f40857a2
Matias Lugo <mlugo@algeiba.com>
2022-11-20 04:38:27 -0300

    Creacion proyecto de test y agrego coverlet. Cambio en docker-image CI

---
+++ UnqMeterAPI.Test/PresentacionServiceTest.cs
@@ -0,0 +1,121 @@
+using AutoMapper;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnqMeterAPI.Interfaces;
+using UnqMeterAPI.Models;
+using UnqMeterAPI.Services;
+
+namespace UnqMeterAPI.Test
+{
+    [TestFixture]
+    public class PresentacionServiceTest
+    {
+        private IPresentacionService? _presentacionService;
+        private Mock<IMapper>? _mockMapper;
+        private Mock<IRepositoryManager<Presentacion>>? _repositoryPresentacionMocker;
+        private Mock<IRepositoryManager<Slyde>>? _repositorySlydeMocker;
+        private Mock<IRepositoryManager<OpcionesSlyde>>? _repositoryOpcionesSlydeMocker;
+
+        IList<Presentacion> presentaciones = new List<Presentacion>();
+        string USUARIO_CREADOR = "practicas.des.soft@mail.com";
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockMapper = new Mock<IMapper>();
+            _repositoryPresentacionMocker = new Mock<IRepositoryManager<Presentacion>>();
+            _repositorySlydeMocker = new Mock<IRepositoryManager<Slyde>>();
+            _repositoryOpcionesSlydeMocker = new Mock<IRepositoryManager<OpcionesSlyde>>();
+
+            _presentacionService = new PresentacionService(_mockMapper.Object, _repositoryPresentacionMocker.Object, _repositorySlydeMocker.Object, _repositoryOpcionesSlydeMocker.Object);
+
+            presentaciones = new List<Presentacion>();
+        }
+
+        [Test]
+        public void GetMisPresentaciones_NoEncuentraDatos()
+        {
+            presentaciones.Add(GetPresentacion());
+            _repositoryPresentacionMocker?.Setup(x => x.GetAll()).Returns(presentaciones.AsQueryable());
+            var result = _presentacionService?.GetMisPresentaciones("nrodriguez@mail.com");
+
+            Assert.AreEqual(0, result?.Count);
+        }
+
+        [Test]
+        public void GetMisPresentaciones_EncuentraDatos()
+        {
+            presentaciones.Add(GetPresentacion());
+            _repositoryPresentacionMocker?.Setup(x => x.GetAll()).Returns(presentaciones.AsQueryable());
+            var result = _presentacionService?.GetMisPresentaciones(USUARIO_CREADOR);
+
+            Assert.AreEqual(1, result?.Count);
+            Assert.AreEqual("Presentacion test", result?.First().nombre);
+        }
+
+        [Test]
+        public void SinFechaSeteada_CompartirPresentacion_SeteaCorrectamenteFechaInicioFin()
+        {
+            presentaciones.Add(GetPresentacion());
+            _repositoryPresentacionMocker?.Setup(x => x.FindBy(x => x.Id == 1)).Returns(presentaciones.AsQueryable());
+            var result = _presentacionService?.CompartirPresentacion(1) != null ? _presentacionService.CompartirPresentacion(1) : new Presentacion();
+
+            Assert.IsNotNull(result?.FechaInicioPresentacion);
+            Assert.IsNotNull(result?.FechaFinPresentacion);
+
+            var startDate = result?.FechaInicioPresentacion != null ? result.FechaInicioPresentacion : DateTime.Now;
+            var endDate = result?.FechaFinPresentacion != null ? result.FechaFinPresentacion : DateTime.Now;
+
+            Assert.AreEqual(startDate.Value.AddDays(1), endDate.Value);
+        }
+
+        [Test]
+        public void ConFechaSeteada_CompartirPresentacion_NoActualizaFecha()
+        {
+            Presentacion presentacion = GetPresentacionConFechas(-2, -1);
+            presentaciones.Add(presentacion);
+            _repositoryPresentacionMocker?.Setup(x => x.FindBy(x => x.Id == 1)).Returns(presentaciones.AsQueryable());
+            var result = _presentacionService?.CompartirPresentacion(1);
+
+            Assert.AreEqual(presentacion.FechaInicioPresentacion, result?.FechaInicioPresentacion);
+            Assert.AreEqual(presentacion.FechaFinPresentacion, result?.FechaFinPresentacion);
+        }
+
+        [Test]
+        public void FechaFinVencida_EstaVencidaLaPresentacion_True()
+        {
+            presentaciones.Add(GetPresentacionConFechas(-2, -1));
+            _repositoryPresentacionMocker?.Setup(x => x.FindBy(x => x.Id == 1)).Returns(presentaciones.AsQueryable());
+            var result = _presentacionService?.EstaVencidaLaPresentacion(1);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void FechaFinVencida_EstaVencidaLaPresentacion_False()
+        {
+            presentaciones.Add(GetPresentacionConFechas(-1, 1));
+            _repositoryPresentacionMocker?.Setup(x => x.FindBy(x => x.Id == 1)).Returns(presentaciones.AsQueryable());
+            var result = _presentacionService?.EstaVencidaLaPresentacion(1);
+
+            Assert.IsFalse(result);
+        }
+
+        private Presentacion GetPresentacion()
+        {
+            return new Presentacion() { UsuarioCreador = USUARIO_CREADOR, Nombre = "Presentacion test", TiempoDeVida = 1, TipoTiempoDeVida = TipoTiempoDeVida.DIA };
+        }
+
+        private Presentacion GetPresentacionConFechas(int cantDiasInicio, int cantDiasFin)
+        {
+            Presentacion presentacion = GetPresentacion();
+            presentacion.FechaInicioPresentacion = DateTime.Now.AddDays(cantDiasInicio);
+            presentacion.FechaFinPresentacion = DateTime.Now.AddDays(cantDiasFin);
+
+            return presentacion;
+        }
+    }
+}
--- UnqMeterAPI.Test/RespuestaParticipanteServiceTest.cs
+++ UnqMeterAPI.Test/RespuestaParticipanteServiceTest.cs
@@ -1,20 +1,22 @@
-﻿using AutoMapper;
-using Moq;
+﻿using Moq;
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 using UnqMeterAPI.Interfaces;
 using UnqMeterAPI.Models;
 using UnqMeterAPI.Services;
+
 namespace UnqMeterAPI.Test
 {
     [TestFixture]
     public class RespuestaParticipanteServiceTest
     {
-        private IRespuestaParticipanteService _respuestaParticipanteService;
-        private Mock<IRepositoryManager<Respuesta>> _repositoryRespuestaMocker;
-        private Mock<IRepositoryManager<DescripcionRespuesta>> _repositoryDescripcionRespuestaMocker;
-        private Mock<IRepositoryManager<Slyde>> _repositorySlydeMocker;
+        private IRespuestaParticipanteService? _respuestaParticipanteService;
+        private Mock<IRepositoryManager<Respuesta>>? _repositoryRespuestaMocker;
+        private Mock<IRepositoryManager<DescripcionRespuesta>>? _repositoryDescripcionRespuestaMocker;
+        private Mock<IRepositoryManager<Slyde>>? _repositorySlydeMocker;
 
-        const string participante = "1.1.1.1"; 
+        const string participante = "1.1.1.1";
 
         [SetUp]
         public void SetUp()
@@ -25,29 +27,34 @@ namespace UnqMeterAPI.Test
 
             _respuestaParticipanteService = new RespuestaParticipanteService(_repositoryRespuestaMocker.Object, _repositorySlydeMocker.Object, _repositoryDescripcionRespuestaMocker.Object);
         }
-  
+
+        public Mock<IRepositoryManager<Slyde>>? Get_repositorySlydeMocker()
+        {
+            return _repositorySlydeMocker;
+        }
+
         [Test]
         public void SlydeConYSinRespuesta_GetSlydesNoRespondidas_SlydesSinRespuesta()
         {
-            _repositorySlydeMocker.Setup(x => x.FindBy(x => x.Presentacion.Id == 1)).Returns(GetSlydesPresentacion1().AsQueryable());
-            _repositoryRespuestaMocker.Setup(x => x.FindBy(x => x.Participante == participante)).Returns(GetRespuestas().AsQueryable());
+            _repositorySlydeMocker?.Setup(x => x.FindBy(x => x.Presentacion.Id == 1)).Returns(GetSlydesPresentacion1().AsQueryable());
+            _repositoryRespuestaMocker?.Setup(x => x.FindBy(x => x.Participante == participante)).Returns(GetRespuestas().AsQueryable());
 
-            var slydesSinRespuesta = _respuestaParticipanteService.GetSlydesSinRespuestas(1, participante);
+            var slydesSinRespuesta = _respuestaParticipanteService?.GetSlydesSinRespuestas(1, participante);
 
-            Assert.AreEqual(2, slydesSinRespuesta.Count);
-            Assert.AreEqual(2, slydesSinRespuesta[0].Id);
-            Assert.AreEqual(3, slydesSinRespuesta[1].Id);
+            Assert.AreEqual(2, slydesSinRespuesta?.Count);
+            Assert.AreEqual(2, slydesSinRespuesta?[0].Id);
+            Assert.AreEqual(3, slydesSinRespuesta?[1].Id);
         }
 
         [Test]
         public void SlydeConRespuesta_GetSlydesNoRespondidas_ListaVacia()
         {
-            _repositorySlydeMocker.Setup(x => x.FindBy(x => x.Presentacion.Id == 2)).Returns(GetSlydesPresentacion2().AsQueryable());
-            _repositoryRespuestaMocker.Setup(x => x.FindBy(x => x.Participante == participante)).Returns(GetRespuestas().AsQueryable());
+            _repositorySlydeMocker?.Setup(x => x.FindBy(x => x.Presentacion.Id == 2)).Returns(GetSlydesPresentacion2().AsQueryable());
+            _repositoryRespuestaMocker?.Setup(x => x.FindBy(x => x.Participante == participante)).Returns(GetRespuestas().AsQueryable());
 
-            var slydesSinRespuesta = _respuestaParticipanteService.GetSlydesSinRespuestas(2, participante);
+            var slydesSinRespuesta = _respuestaParticipanteService?.GetSlydesSinRespuestas(2, participante);
 
-            Assert.AreEqual(0, slydesSinRespuesta.Count);
+            Assert.AreEqual(0, slydesSinRespuesta?.Count);
         }
 
         private List<Slyde> GetSlydesPresentacion1()
@@ -69,16 +76,16 @@ namespace UnqMeterAPI.Test
             List<Slyde> slydes = new List<Slyde>();
             slydes.Add(new Slyde() { Presentacion = presentacion, Id = 1 });
 
-            return slydes; 
+            return slydes;
         }
 
         private List<Respuesta> GetRespuestas()
         {
             List<Respuesta> respuestas = new List<Respuesta>();
 
-            respuestas.Add(new Respuesta() { Id = 1, Slyde = new Slyde() { Id = 1 }, Participante= "1.1.1.1" });
+            respuestas.Add(new Respuesta() { Id = 1, Slyde = new Slyde() { Id = 1 }, Participante = "1.1.1.1" });
 
-            return respuestas; 
+            return respuestas;
         }
     }
 }
